0040f04960d540ba3c31b5a77ebb9917ddc28cc7
Zaur <zaur_aliev@hotmail.com>
2025-12-22 04:28:22 -0500

    New Updates Code base and Upgraded NPM, Yarn and Webpack

--- src/statics/data.js
+++ src/statics/data.js
@@ -1,15 +1,18 @@
 const data = [
   { 
     name: "0001-app",
-    id: "9eb2fd3c-809b-4e04-9ad4-56574cfdb545" //uuidv4()
+    id: "9eb2fd3c-809b-4e04-9ad4-56574cfdb545", //uuidv4()
+    isSynced: false,
   },
   { 
     name: "0002-app",
-    id: "4e9ef95e-a1bb-4049-90e0-32d5e48b24c9" //uuidv4()
+    id: "4e9ef95e-a1bb-4049-90e0-32d5e48b24c9", //uuidv4()
+    isSynced: false,
   },
   { 
     name: "0003-app",
-    id: "c5599c42-57fe-4045-87ca-4696de9900c9" //uuidv4()
+    id: "c5599c42-57fe-4045-87ca-4696de9900c9", //uuidv4()
+    isSynced: false,
   },
 ];
 
--- src/util/util-functions.js
+++ src/util/util-functions.js
@@ -1,10 +1,17 @@
-"use strict"
+"use strict";
 
 const uuidv4 = function () {
-  return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-    (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
+  return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (
+      c ^
+      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+    ).toString(16)
   );
-}
+};
+
+const newUUID = function () {
+  return crypto.randomUUID();
+};
 
 function constructElement(elementProps, elementType) {
   let element = document.createElement(elementType);
@@ -14,4 +21,4 @@ function constructElement(elementProps, elementType) {
   return element;
 }
 
-export { uuidv4, constructElement };
+export { uuidv4, constructElement, newUUID };
